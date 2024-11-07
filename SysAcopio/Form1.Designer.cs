88d9ee6e8e0a049ab32ef53d787bbf82d0823e24
REMR11 <remr11@ufg.edu.sv>
2024-11-07 15:53:59 -0600

    Feat: Cambio de posicion xd

--- SysAcopio/Form1.Designer.cs
+++ SysAcopio/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(26, 220);
+            this.dataGridView1.Location = new System.Drawing.Point(30, 47);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(1032, 183);
             this.dataGridView1.TabIndex = 0;
