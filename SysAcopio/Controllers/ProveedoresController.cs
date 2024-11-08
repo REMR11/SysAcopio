d2add0314ee42251291a170427c2770ce77e8fe0
DarkKingpro10 <jesusgogeta@outlook.com>
2024-11-08 16:31:14 -0600

    feat:Repository y Controller de Proveedores más Generic Functions y modal todo feo de alertas

---
+++ SysAcopio/Controllers/ProveedoresController.cs
@@ -0,0 +1,101 @@
+﻿using SysAcopio.Models;
+using SysAcopio.Repositories;
+using SysAcopio.Utils;
+using System.Data;
+
+namespace SysAcopio.Controllers
+{
+    internal class ProveedoresController
+    {
+        private readonly ProveedorRepository repository;
+        private readonly Alerts alerts;
+
+        /// <summary>
+        /// Constructor de la clase de Proveedores controller
+        /// </summary>
+        public ProveedoresController()
+        {
+            repository = new ProveedorRepository();
+            alerts = new Alerts();
+        }
+
+        /// <summary>
+        /// Método para obtener todos los registros
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        /// <summary>
+        /// Método para crear un Proveedor
+        /// </summary>
+        /// <param name="proveedor"></param>
+        public void Create(Proveedor proveedor)
+        {
+            long id = repository.Create(proveedor);
+
+            if (id > 0)
+            {
+                alerts.ShowAlert("Proveedor creado", AlertsType.Confirm);
+            }
+            else
+            {
+                alerts.ShowAlert("Lo sentimos, debido a un errror no se pudo crear el proveedor", AlertsType.Error);
+            }
+        }
+
+        /// <summary>
+        /// Método para actualizar un Proveedor
+        /// </summary>
+        /// <param name="proveedor"></param>
+        /// <returns>Un booleano que confirma la modificación</returns>
+        public bool Modify(Proveedor proveedor)
+        {
+            bool result = repository.Update(proveedor);
+
+            if (result)
+            {
+                alerts.ShowAlert("Proveedor modificado", AlertsType.Confirm);
+            }
+            else
+            {
+                alerts.ShowAlert("Lo sentimos, debido a un errror no se pudo modificar el proveedor", AlertsType.Error);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Método para eliminar un Proveedor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Booleano que confirma la eliminación</returns>
+        public bool Delete(long id)
+        {
+            int result = repository.DeleteLogic(id);
+
+            if (result == -1)
+            {
+                alerts.ShowAlert("Proveedor inexistente", AlertsType.Info);
+                return false;
+            }
+            else if (result == 0)
+            {
+                alerts.ShowAlert("Ocurrio un error y no se pudo borrar el registro", AlertsType.Error);
+                return false;
+            }
+            else
+            {
+                alerts.ShowAlert("Proveedor borrado con exito, aun existarán los registros que se registraron con este proveedor, se recomienda que se actualicen", AlertsType.Confirm);
+                return true;
+            }
+        }
+
+        public DataTable Search(string searchQuery)
+        {
+            return repository.SearchProveedores(searchQuery);
+        }
+    }
+}
---
+++ SysAcopio/Views/AlertForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SysAcopio.Views
+{
+    partial class AlertForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.picIcono = new System.Windows.Forms.PictureBox();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcono)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(153, 123);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(176, 48);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "lblTitulo";
+            // 
+            // picIcono
+            // 
+            this.picIcono.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picIcono.Location = new System.Drawing.Point(156, 12);
+            this.picIcono.Name = "picIcono";
+            this.picIcono.Size = new System.Drawing.Size(171, 108);
+            this.picIcono.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picIcono.TabIndex = 1;
+            this.picIcono.TabStop = false;
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMensaje.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtMensaje.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensaje.Location = new System.Drawing.Point(1, 167);
+            this.txtMensaje.Multiline = true;
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.ReadOnly = true;
+            this.txtMensaje.Size = new System.Drawing.Size(480, 83);
+            this.txtMensaje.TabIndex = 2;
+            this.txtMensaje.TabStop = false;
+            this.txtMensaje.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AlertForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 253);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.picIcono);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "AlertForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.picIcono)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox picIcono;
+        private System.Windows.Forms.TextBox txtMensaje;
+    }
+}
