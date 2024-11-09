7e38ab890f1554964795c5dfba8a10b124f80451
DarkKingpro10 <jesusgogeta@outlook.com>
2024-11-08 19:35:49 -0600

    feat: Preparando los repositorios y controllers de Donaciones y RecursoDonacion

--- SysAcopio/Controllers/DonacionesController.cs
+++ SysAcopio/Controllers/DonacionesController.cs
@@ -1,26 +1,81 @@
 ﻿using SysAcopio.Models;
 using SysAcopio.Repositories;
+using SysAcopio.Utils;
+using System.Collections.Generic;
+using System.Data;
 
 namespace SysAcopio.Controllers
 {
     public class DonacionesController
     {
         private readonly DonacionRepository donacionRepository;
+        private readonly Alerts alerts;
+        public List<RecursoDonacion> DetalleRecursosDonacion { get; set; }
 
-        public long Create(Donacion donacion)
+        /// <summary>
+        /// Contructor parametrizado
+        /// </summary>
+        public DonacionesController()
         {
-            return donacionRepository.Create(donacion);
+            donacionRepository = new DonacionRepository();
+            alerts = new Alerts();
         }
 
-        public void Update(Donacion donacion)
+        /// <summary>
+        /// Método que obtiene todas las donaciones
+        /// </summary>
+        /// <returns>Objeto de tipo DataTable que contiene las donaciones</returns>
+        public DataTable GetDonaciones()
         {
-            donacionRepository.update(donacion);
+            return donacionRepository.GetAll();
         }
 
-        public void Delete(long id)
+        /// <summary>
+        /// Método que verificar si una donación existe
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Valor booleano que confirma la existencia</returns>
+        public Donacion GetDonacionById(long id)
         {
-            donacionRepository.delete(id);
+            return donacionRepository.GetById(id);
         }
 
+        /// <summary>
+        /// Método que agrega un detalle a la lista
+        /// </summary>
+        /// <returns></returns>
+        public void AddDetalle(RecursoDonacion recursoDonacion)
+        {
+            DetalleRecursosDonacion.Add(recursoDonacion);
+        }
+
+        /// <summary>
+        /// Método que crea las Donaciones y su detalle
+        /// </summary>
+        /// <returns></returns>
+        public bool Create(Donacion donacion)
+        {
+            //Creamos la donación
+            long id = donacionRepository.Create(donacion);
+
+            //Validamos que si se halla creado
+            if (id < 0)
+            {
+                alerts.ShowAlert("Hubo al crear la Donación", AlertsType.Error);
+                return false;
+            }
+
+            RecursoDonacionController recursoDonacionController = new RecursoDonacionController();
+            //Ahora creamos el detalle
+            foreach (var elementoDetalle in DetalleRecursosDonacion)
+            {
+                if (!recursoDonacionController.CreateDetail(elementoDetalle, id))
+                {
+                    alerts.ShowAlert($"Ocurrio un error al agregar el recurso {elementoDetalle.IdRecurso} al detalle en la base de datos", AlertsType.Error);
+                }
+            }
+            alerts.ShowAlert("Felicidades has recibido un donación, tus recursos se han actualizado automaticamente", AlertsType.Confirm);
+            return true;
+        }
     }
 }
---
+++ SysAcopio/Controllers/RecursoDonacionController.cs
@@ -0,0 +1,46 @@
+﻿using SysAcopio.Models;
+using SysAcopio.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysAcopio.Controllers
+{
+    internal class RecursoDonacionController
+    {
+        private readonly RecursoDonacionRepository repository;
+
+        /// <summary>
+        /// Constructor que inicializa el repositorio
+        /// </summary>
+        public RecursoDonacionController()
+        {
+            repository = new RecursoDonacionRepository();
+        }
+
+        /// <summary>
+        /// Método que retorna una tabla con todos los registros del detalle de la Donacion
+        /// </summary>
+        /// <param name="IdDonacion"></param>
+        public DataTable GetDetailDonation(long IdDonacion)
+        {
+            return repository.GetDetailDonation(IdDonacion);
+        }
+
+        public bool CreateDetail(RecursoDonacion recursoDonacion, long idDonacion)
+        {
+            //Validamos que la donación exista
+            DonacionesController donacionesController = new DonacionesController();
+            if (donacionesController.GetDonacionById(idDonacion) != null)
+            {
+                return repository.Create(recursoDonacion, idDonacion) > 0;
+            }
+
+            return false;
+        }
+    }
+}
