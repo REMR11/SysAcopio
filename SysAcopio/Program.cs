2f4935c7b69edc62825caeca3db82ae7ba633cc5
REMR11 <remr11@ufg.edu.sv>
2024-11-23 17:37:20 -0600

    merge

--- SysAcopio/Program.cs
+++ SysAcopio/Program.cs
@@ -17,8 +17,7 @@ namespace SysAcopio
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-            //Application.Run(new Login());
+            Application.Run(new Login());
 
         }
     }
